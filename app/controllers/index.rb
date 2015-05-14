87cc8f8514a5c5db3d5f33ee36a2eeeab2461fd5
Mark <markdmcq@gmail.com>
2015-05-13 19:05:29 -0700

    get price and image from Amazon search

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,25 +4,37 @@ get '/' do
 	"hello world"
 	request = Vacuum.new
 
-	request.configure(
-    aws_access_key_id:
-    aws_secret_access_key:
-    associate_tag:
-)
+# 	request.configure(
+#     aws_access_key_id:
+#     aws_secret_access_key:
+#     associate_tag:
+# )
 
 
 
 
-output = request.item_search(
-  query: {
-    'Keywords' => 'R2D2',
-    'SearchIndex' => 'All',
-    'ItemPage' => '1',
-    'ItemSearch.Shared.ResponseGroup' => 'Large',
-  }
-)
 
-p output.to_h
+  output = request.item_search(
+    query: {
+      'Keywords' => 'harry potter first book',
+      'SearchIndex' => 'All',
+      'ItemPage' => '1',
+      'ItemSearch.Shared.ResponseGroup' => 'Large',
+    }
+  )
+  # This let's you see the hash reall pretty like:
+  # data = output.to_h.to_json
 
+  # This let's you access the hash:
+  data = output.to_h
+  @photo_link = data['ItemSearchResponse']['Items']['Item'].first['LargeImage']['URL']
+  price_link = data['ItemSearchResponse']['Items']['Item'].first
+
+  price = price_link['Offers']['Offer']['OfferListing']['Price']['FormattedPrice'].to_json
+
+  @price = price.gsub /"/, ''
+  # ['Offers']]['Offer']['OfferListing']
+
+  erb :show
 
 end
